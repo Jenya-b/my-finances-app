f95d11a80e620ef1541c08c921d3dce7cd803880
jenya-b <evgeny_rab@inbox.ru>
2023-04-01 13:49:16 +0300

    fix: testing render App

--- src/modules/components/App/App.test.tsx
+++ src/modules/components/App/App.test.tsx
@@ -1,6 +1,9 @@
 import { render } from '@testing-library/react';
+
 import { App } from './App';
 
-test('renders App', () => {
-  render(<App />);
+test('renders App', async () => {
+  const tree = render(<App />);
+
+  expect(await tree.findAllByText(/отложено/i)).toHaveLength(1);
 });
